5fafc2e1d447470ac48c48ea76aa2a203b6b6d1f
Crafton Williams <740320+crafton@users.noreply.github.com>
2021-07-26 21:03:07 +1000

    remove magic link from build package

--- .github/scripts/package_plugin.sh
+++ .github/scripts/package_plugin.sh
@@ -3,5 +3,5 @@
 set -e
 
 mkdir "$PACKAGE_NAME"
-cp -r nt-postman.php LICENSE CHANGELOG.md tile site-link magic-link documentation version-control.json "$PACKAGE_NAME"/
+cp -r nt-postman.php LICENSE CHANGELOG.md tile site-link documentation version-control.json "$PACKAGE_NAME"/
 zip -r "$PACKAGE_NAME".zip "$PACKAGE_NAME"
